9ef1475b53b06de5e317e970088a3d5ce9f28a05
Nicolly Isadora Vieira Prioste <nicolly.prioste@escola.pr.gov.br>
2025-09-25 12:37:37 -0300

    script

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 [
-{
-    nome: "O Senhor dos Anéis",
-    imagem: "https://upload.wikimedia.org/wikipedia/pt/8/87/The_Lord_of_the_Rings_poster.jpg",
-    trailer: "https://www.youtube.com/watch?v=V75dMMIW2B4"
+  {
+    "nome": "O Senhor dos Anéis",
+    "imagem": "https://upload.wikimedia.org/wikipedia/pt/8/87/The_Lord_of_the_Rings_poster.jpg",
+    "trailer": "https://www.youtube.com/watch?v=V75dMMIW2B4"
   },
   {
-    nome: "Harry Potter",
-    imagem: "https://upload.wikimedia.org/wikipedia/pt/6/6e/Harry_Potter_Pedra_Filosofal_2001.jpg",
-    trailer: "https://www.youtube.com/watch?v=VyHV0BRtdxo"
+    "nome": "Harry Potter",
+    "imagem": "https://upload.wikimedia.org/wikipedia/pt/6/6e/Harry_Potter_Pedra_Filosofal_2001.jpg",
+    "trailer": "https://www.youtube.com/watch?v=VyHV0BRtdxo"
   },
   {
-    nome: "Vingadores",
-    imagem: "https://upload.wikimedia.org/wikipedia/pt/f/f9/TheAvengers2012Poster.jpg",
-    trailer: "https://www.youtube.com/watch?v=eOrNdBpGMv8"
+    "nome": "Vingadores",
+    "imagem": "https://upload.wikimedia.org/wikipedia/pt/f/f9/TheAvengers2012Poster.jpg",
+    "trailer": "https://www.youtube.com/watch?v=eOrNdBpGMv8"
   },
   {
-    nome: "Titanic",
-    imagem: "https://upload.wikimedia.org/wikipedia/pt/2/22/Titanic_poster.jpg",
-    trailer: "https://www.youtube.com/watch?v=CHekzSiZjrY"
+    "nome": "Titanic",
+    "imagem": "https://upload.wikimedia.org/wikipedia/pt/2/22/Titanic_poster.jpg",
+    "trailer": "https://www.youtube.com/watch?v=CHekzSiZjrY"
   },
   {
-    nome: "Matrix",
-    imagem: "https://upload.wikimedia.org/wikipedia/pt/c/c1/The_Matrix_Poster.jpg",
-    trailer: "https://www.youtube.com/watch?v=m8e-FF8MsqU"
+    "nome": "Matrix",
+    "imagem": "https://upload.wikimedia.org/wikipedia/pt/c/c1/The_Matrix_Poster.jpg",
+    "trailer": "https://www.youtube.com/watch?v=m8e-FF8MsqU"
   }
-];
+]
 
 const searchInput = document.getElementById("search");
 const resultsDiv = document.getElementById("results");
